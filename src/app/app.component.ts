3295209196bd2fa465bdc26f0e32a07bf6fd61fd
Karaan Nannda <karaan.nannda@gmail.com>
2017-10-10 19:39:21 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ export class DashboardComponent implements OnInit {
 
   range =[];
   rplus1 =[];
+  rminus1 =[];
   rminus2 = [];
   rplus2 = [];
   ravg = [];
+  value = [];
 
 
   constructor(
@@ -24,167 +26,7 @@ export class DashboardComponent implements OnInit {
     line.height = 200;
     line.width = 200;
 
-    let dump = [{
-      "x": 1,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 3.3,
-      "plus1": 6.906937759374286,
-      "plus2": 10.513875518748574
-    }, {
-      "x": 2,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 4,
-      "average20": 3.45,
-      "plus1": 7.02036412708844,
-      "plus2": 10.59072825417688
-    }, {
-      "x": 3,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 3.45,
-      "plus1": 7.02036412708844,
-      "plus2": 10.59072825417688
-    }, {
-      "x": 4,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 4,
-      "average20": 3.25,
-      "plus1": 6.668698582794336,
-      "plus2": 10.087397165588673
-    }, {
-      "x": 5,
-      "minus2": 0,
-      "minus1": 0.017693094942448084,
-      "value": 4,
-      "average20": 3.4,
-      "plus1": 6.782306905057552,
-      "plus2": 10.164613810115103
-    }, {
-      "x": 6,
-      "minus2": 0,
-      "minus1": 0.017693094942448084,
-      "value": 1,
-      "average20": 3.4,
-      "plus1": 6.782306905057552,
-      "plus2": 10.164613810115103
-    }, {
-      "x": 7,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 3.05,
-      "plus1": 6.2976914878109955,
-      "plus2": 9.54538297562199
-    }, {
-      "x": 8,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.7,
-      "plus1": 5.767572330035594,
-      "plus2": 8.835144660071187
-    }, {
-      "x": 9,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.7,
-      "plus1": 5.767572330035594,
-      "plus2": 8.835144660071187
-    }, {
-      "x": 10,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.7,
-      "plus1": 5.767572330035593,
-      "plus2": 8.835144660071187
-    }, {
-      "x": 11,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.35,
-      "plus1": 5.183284313301438,
-      "plus2": 8.016568626602876
-    }, {
-      "x": 12,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.35,
-      "plus1": 5.183284313301438,
-      "plus2": 8.016568626602876
-    }, {
-      "x": 13,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.35,
-      "plus1": 5.183284313301438,
-      "plus2": 8.016568626602876
-    }, {
-      "x": 14,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.35,
-      "plus1": 5.183284313301438,
-      "plus2": 8.016568626602876
-    }, {
-      "x": 15,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2.35,
-      "plus1": 5.183284313301438,
-      "plus2": 8.016568626602876
-    }, {
-      "x": 16,
-      "minus2": 0,
-      "minus1": 0,
-      "value": 1,
-      "average20": 2,
-      "plus1": 4.529822128134704,
-      "plus2": 7.059644256269407
-    }, {
-      "x": 17,
-      "minus2": 0,
-      "minus1": 0.37878573571857244,
-      "value": 1,
-      "average20": 1.45,
-      "plus1": 2.5212142642814275,
-      "plus2": 3.5924285285628548
-    }, {
-      "x": 18,
-      "minus2": 0,
-      "minus1": 0.37878573571857244,
-      "value": 1,
-      "average20": 1.45,
-      "plus1": 2.5212142642814275,
-      "plus2": 3.5924285285628548
-    }, {
-      "x": 19,
-      "minus2": 0,
-      "minus1": 0.37878573571857266,
-      "value": 1,
-      "average20": 1.45,
-      "plus1": 2.5212142642814275,
-      "plus2": 3.5924285285628548
-    }, {
-      "x": 20,
-      "minus2": 0,
-      "minus1": 0.37878573571857266,
-      "value": 1,
-      "average20": 1.45,
-      "plus1": 2.5212142642814275,
-      "plus2": 3.5924285285628548
-    }];
+    let dump = [{"x":1,"minus2":1,"minus1":3,"value":6,"average20":5,"plus1":7,"plus2":9},{"x":2,"minus2":1,"minus1":3,"value":6,"average20":5,"plus1":7,"plus2":9},{"x":3,"minus2":1,"minus1":3,"value":6,"average20":5,"plus1":7,"plus2":9},{"x":4,"minus2":1,"minus1":3,"value":6,"average20":5,"plus1":7,"plus2":9},{"x":5,"minus2":0.41987298107780635,"minus1":2.584936490538903,"value":1,"average20":4.75,"plus1":6.915063509461097,"plus2":9.080127018922195},{"x":6,"minus2":0.41987298107780635,"minus1":2.584936490538903,"value":6,"average20":4.75,"plus1":6.915063509461097,"plus2":9.080127018922195},{"x":7,"minus2":0.41987298107780635,"minus1":2.584936490538903,"value":6,"average20":4.75,"plus1":6.915063509461097,"plus2":9.080127018922195},{"x":8,"minus2":1,"minus1":3,"value":6,"average20":5,"plus1":7,"plus2":9},{"x":9,"minus2":1.6792857857285748,"minus1":3.4646428928642874,"value":6,"average20":5.25,"plus1":7.035357107135713,"plus2":8.820714214271426},{"x":10,"minus2":1.6792857857285748,"minus1":3.4646428928642874,"value":1,"average20":5.25,"plus1":7.035357107135713,"plus2":8.820714214271426},{"x":11,"minus2":1.6792857857285748,"minus1":3.4646428928642874,"value":6,"average20":5.25,"plus1":7.035357107135713,"plus2":8.820714214271426},{"x":12,"minus2":1.6792857857285748,"minus1":3.4646428928642874,"value":6,"average20":5.25,"plus1":7.035357107135713,"plus2":8.820714214271426},{"x":13,"minus2":1.6792857857285748,"minus1":3.4646428928642874,"value":1,"average20":5.25,"plus1":7.035357107135713,"plus2":8.820714214271426},{"x":14,"minus2":1,"minus1":3,"value":1,"average20":5,"plus1":7,"plus2":9},{"x":15,"minus2":0.41987298107780635,"minus1":2.584936490538903,"value":1,"average20":4.75,"plus1":6.915063509461097,"plus2":9.080127018922195},{"x":16,"minus2":-0.08257569495583983,"minus1":2.20871215252208,"value":1,"average20":4.5,"plus1":6.7912878474779195,"plus2":9.082575694955839},{"x":17,"minus2":-0.5196960070847281,"minus1":1.865151996457636,"value":1,"average20":4.25,"plus1":6.6348480035423645,"plus2":9.019696007084729},{"x":18,"minus2":-0.8989794855663558,"minus1":1.5505102572168221,"value":1,"average20":4,"plus1":6.449489742783178,"plus2":8.898979485566356},{"x":19,"minus2":-1.2249371855330997,"minus1":1.2625314072334501,"value":1,"average20":3.75,"plus1":6.23746859276655,"plus2":8.7249371855331},{"x":20,"minus2":-1.5,"minus1":1,"value":1,"average20":3.5,"plus1":6,"plus2":8.5}];
 
     const colors = {
       green: {
@@ -197,22 +39,32 @@ export class DashboardComponent implements OnInit {
        // stroke: '#6fccdd',
       },
       darkBlue: {
-        fill: 'rgba(179,181,198,0.2)',
+        fill: 'rgba(179,181,198,0.9)',
       //  stroke: '#3282bf',
       },
       purple: {
         fill: 'rgba(179,181,198,0.5)',
        // stroke: '#75539e',
       },
+      speed: {
+        fill: 'rgb(147, 219, 112)',
+        // stroke: '#75539e',
+      },
+      avg: {
+        fill: 'rgb(255,105,97)',
+        // stroke: '#75539e',
+      }
     };
 
     for (var i = 0; i < dump.length; i++) {
       // console.log(dump[i].average20);
       this.range.push(dump[i].x);
       this.rminus2.push(dump[i].minus2);
+      this.rminus1.push(dump[i].minus1)
       this.rplus1.push(dump[i].plus1);
-      this.rplus2.push(dump[i].plus2)
-      this.ravg.push(dump[i].average20)
+      this.rplus2.push(dump[i].plus2);
+      this.ravg.push(dump[i].average20);
+      this.value.push(dump[i].value);
     }
 
     let karaan = this.range.toString();
@@ -220,20 +72,22 @@ export class DashboardComponent implements OnInit {
     let data = {
       labels: this.range,
       datasets: [
-        {
 
-          "data": this.range,   // Example data
-          fill: true,
-          backgroundColor: colors.purple.fill,
-
-          label: "x"
-        },
         {
           "data": this.rplus1,   // Example data
           fill: true,
           backgroundColor: colors.darkBlue.fill,
 
           label: "plus1"
+
+        },
+
+        {
+          "data": this.rminus1,   // Example data
+          fill: true,
+          backgroundColor: colors.darkBlue.fill,
+
+          label: "minus1"
 
         },
         {
@@ -243,19 +97,27 @@ export class DashboardComponent implements OnInit {
 
           label: "plus2"
         },
+
         {
           "data": this.rminus2,   // Example data
           fill: true,
-          backgroundColor: colors.green.fill,
+          backgroundColor: colors.darkBlue.fill,
 
           label: "minus2"
         },
         {
           "data": this.ravg,   // Example data
-          fill: true,
-          backgroundColor: colors.green.fill,
+          fill: false,
+          backgroundColor: colors.avg.fill,
 
-          label: "avg"
+          label: "Average"
+        },
+        {
+          "data": this.value,   // Example data
+          fill: false,
+          backgroundColor: colors.speed.fill,
+
+          label: "speed"
         }
 
 
@@ -278,7 +140,7 @@ export class DashboardComponent implements OnInit {
 
           scales: {
             yAxes: [{
-              stacked: true,
+              stacked: false,
             }]
           },
           "animation": {
